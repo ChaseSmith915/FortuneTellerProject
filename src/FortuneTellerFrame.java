d91d921d275028224de00dd3353458de8d734a55
Chase Smith <chaseosmith915@gmail.com>
2022-09-26 21:19:16 -0400

    final

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -20,21 +20,18 @@ public class FortuneTellerFrame extends JFrame
     JButton quitButton;
 
     String[] fortunes = {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "11",
-            "12",
-            "13",
-            "14",
-            "15"
+            "At least some time in your life, your program will work on the first attempt",
+            "You'll click the fortune button again to get another fortune",
+            "A foolish man listens to his heart. A wise man listens to me, Zoltar!",
+            "You'll live long enough to click that button at least 5 more times",
+            "Never forget your siblings, especially if they owe you",
+            "Another student will make this exact fortune",
+            "No, there's no cookie, only fortune",
+            "You've been blessed with the wisdom of reading",
+            "You'll live on to read many more fortunes, but only this one matters",
+            "Your shoe's untied",
+            "This fortune is worth a fortune",
+            "You will not see this fortune twice in a row"
     };
     int curFortuneDex = -1;
     Random rnd = new Random();
@@ -44,7 +41,7 @@ public class FortuneTellerFrame extends JFrame
         mainPnl = new JPanel();
         mainPnl.setLayout(new BorderLayout());
         createTopPanel();
-        //mainPnl.add(topPnl, BorderLayout.NORTH);
+        mainPnl.add(topPnl, BorderLayout.NORTH);
         createMiddlePanel();
         mainPnl.add(midPnl, BorderLayout.CENTER);
         createBottomPanel();
@@ -59,7 +56,14 @@ public class FortuneTellerFrame extends JFrame
 
     private void createTopPanel()
     {
+        topPnl = new JPanel();
+        icon = new ImageIcon("src/zoltar.png");
+        titleLbl = new JLabel("Fortune Teller", icon, JLabel.CENTER);
+        titleLbl.setVerticalTextPosition(JLabel.BOTTOM);
+        titleLbl.setHorizontalTextPosition(JLabel.CENTER);
+        titleLbl.setFont(new Font("SansSerif", Font.BOLD, 38));
 
+        topPnl.add(titleLbl);
     }
     private void createMiddlePanel()
     {
